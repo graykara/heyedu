bc2b27fc58086ddaada704471eed08538cb61107
진기정 <graykara@nate.com>
2022-05-23 12:12:08 +0900

    devise, pundit 설정, tailwind plugin 추가
    pundit 권한 기본 설정
    
    tailwind 관련 plugin 추가

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Pundit
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :render_forbidden
+
+  def render_forbidden
+    render file: Rails.root.join('public', '403.html'), status: :forbidden, layout: false, content_type: 'text/html'
+  end
 end
